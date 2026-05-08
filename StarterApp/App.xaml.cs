3b22afa522311086beeeb2fc8907121d9d73305d
elliethenerd <elliejohnston127@gmail.com>
2026-05-08 18:39:55 +0100

    Final coursework version

--- StarterApp/App.xaml.cs
+++ StarterApp/App.xaml.cs
@@ -20,13 +20,12 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		// var window = base.CreateWindow(activationState);
-		// window.Page = new AppShell();
+
 
 		var shell = _serviceProvider.GetService<AppShell>();
 		if (shell == null)
 		{
-			// Handle the error if AppShell could not be resolved
+
 			throw new InvalidOperationException("AppShell could not be resolved from the service provider.");
 		}
 		var window = new Window(shell);
